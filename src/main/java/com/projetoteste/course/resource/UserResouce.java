874b538dd9ae57d0457722b4f91d9afd2cfc2a38
Cesar Oliveira Jr <cesar.junior@certsys.com.br>
2024-07-30 22:47:18 -0300

    Order, Instant, ISO 8601 - part1

--- src/main/java/com/projetoteste/course/resource/UserResouce.java
+++ src/main/java/com/projetoteste/course/resource/UserResouce.java
@@ -1,18 +1,31 @@
 package com.projetoteste.course.resource;
 
 import com.projetoteste.course.entities.User;
+import com.projetoteste.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResouce {
+    @Autowired
+    private UserService service;
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findByID(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }
